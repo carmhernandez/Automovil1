8872b3b7b2f790881902f027d4c32cfe627eed6c
carmhernandez <carmencita1284@hotmail.com>
2014-09-28 00:38:37 -0500

    subiendo archivos

---
+++ AbstractFactoryAutomovil/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AbstractFactoryAutomovil.mundo.interfaces;
+using AbstractFactoryAutomovil.mundo.clases;
+using AbstractFactoryAutomovil.mundo.fabricas;
+
+namespace AbstractFactoryAutomovil
+{
+    public partial class Cliente : Form
+    {
+        private IFabricaDePartes fabrica;
+        private IAuto auto;
+        private IMotor motor;
+        private ICarroceria carroceria;
+
+        public Cliente()
+        {
+            InitializeComponent();
+            lstPartes.SelectedIndex = 0;
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            if (radMazda.Checked)
+                fabrica = new FabricaMazda();
+            if (radChevrolet.Checked)
+                fabrica = new FabricaChevrolet();
+            if (radRenault.Checked)
+                fabrica = new FabricaRenault();
+            auto = fabrica.crearAuto();
+            motor = fabrica.crearMotor();
+            carroceria = fabrica.crearCarroceria();
+            if (lstPartes.SelectedIndex == 0)
+                MessageBox.Show(auto.darNombre());
+            else if (lstPartes.SelectedIndex == 1)
+                MessageBox.Show(motor.darNombre());
+            else if (lstPartes.SelectedIndex == 2)
+                MessageBox.Show(carroceria.darNombre());
+        }
+
+        private void radMazda_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
